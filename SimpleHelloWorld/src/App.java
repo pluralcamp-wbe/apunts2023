0662802ce8c54e2b33dda84f8637fd5c055db196
Oriol Boix Anfosso <orboan@pluralcamp.com>
2023-05-03 15:04:19 +0000

    updated

---
+++ SimpleHelloWorld/src/App.java
@@ -0,0 +1,19 @@
+
+public class App {
+	
+	//main method
+	public static void main(String[] args) {
+		//System.out.println("Hello Java !!!");
+		
+		//testeo la clase Car
+		Car coche = new Car();
+		coche.marca = "Nisan";
+		System.out.println(coche.toString());
+		
+		//testeo de la clase Byke
+		
+		
+		
+	} 
+
+}
---
+++ SimpleHelloWorld/src/Car.java
@@ -0,0 +1,8 @@
+
+public class Car {
+	String marca;
+	
+	public String toString() {
+		return "Car brand is " + this.marca;
+	}
+}
