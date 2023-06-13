7f003c931ab2798fded15ff45c4b9fe016fd139f
Oriol Boix Anfosso <orboan@pluralcamp.com>
2023-06-13 11:58:58 +0000

    updated

--- backend/servlets/restaurant-servlets-demo/src/main/java/com/pluralcamp/App.java
+++ backend/servlets/restaurant-servlets-demo/src/main/java/com/pluralcamp/App.java
@@ -9,9 +9,37 @@ public final class App {
 
     /**
      * Says hello to the world.
+     * 
      * @param args The arguments of the program.
      */
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        String webappDirLocation = "src/main/webapp/";
+        Tomcat tomcat = new Tomcat();
+        tomcat.setPort(8082);
+
+        // https://tomcat.apache.org/tomcat-10.0-doc/connectors.html
+        /*
+         * The HTTP Connector element represents a Connector component
+         * that supports the HTTP/1.1 protocol. It enables Catalina to
+         * function as a stand-alone web server, in addition to
+         * its ability to execute servlets and JSP pages.
+         * 
+         * Mandatory since Tomcat 9
+         * https://stackoverflow.com/questions/56668892/embedded-tomcat-java-application
+         * -is-running-but-server-cannot-be-reached
+         */
+        tomcat.getConnector();
+
+        StandardContext ctx = (StandardContext) tomcat.addWebapp("/",
+                new File(webappDirLocation).getAbsolutePath());
+
+        File additionWebInfClasses = new File("target/classes");
+        WebResourceRoot resources = new StandardRoot(ctx);
+        resources.addPreResources(new DirResourceSet(resources, "/WEB-INF/classes",
+                additionWebInfClasses.getAbsolutePath(), "/"));
+        ctx.setResources(resources);
+
+        tomcat.start();
+        tomcat.getServer().await();
     }
 }
